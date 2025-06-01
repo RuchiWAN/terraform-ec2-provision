5fafb84eae75a09e004f1b2264e03c12ff38218e
RuchiWAN <lucianstoichitoiu@gmail.com>
2025-06-01 21:41:40 +0100

    chore: specify AWS provider version in Terraform config

--- first-ec2.tf
+++ first-ec2.tf
@@ -1,3 +1,12 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.99.1"  # or whatever stable version you're happy with
+    }
+  }
+}
+
 provider "aws" {
   region     = "eu-west-2"
 }
@@ -12,12 +21,12 @@ tags = {
 
 }
 
-provider "github" {
-  token = var.github_token
-}
-resource "github_repository" "example" {
-  name = "example"
-  description = "mine"
-  visibility = "public"
-}
+# provider "github" {
+  # token = var.github_token
+
+# resource "github_repository" "example" {
+  #name = "example"
+  #description = "mine"
+  #visibility = "public"
+
 
